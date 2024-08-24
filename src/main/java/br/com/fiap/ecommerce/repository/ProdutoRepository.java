c07b4a7285aef997f74a80b078aa062212bd24a5
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-23 21:00:13 -0300

    Adicionar model + repository correto

--- src/main/java/br/com/fiap/ecommerce/repository/ProdutoRepository.java
+++ src/main/java/br/com/fiap/ecommerce/repository/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package br.com.fiap.ecommerce.repository;
 
-public class ProdutoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import br.com.fiap.ecommerce.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
 }
